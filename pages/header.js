d1aa15f9df62fd559f89bacdd2835ff6b50283d6
ashish kumar singh <ashishkumarsinghh@outlook.com>
2020-10-20 22:47:25 +0530

    setup nextjs ssr, bootstrap

---
+++ pages/header.js
@@ -0,0 +1,3 @@
+export default function Header(props){
+    return (<h1 style={{textAlign:"center", paddingTop: "1vh", paddingBottom: "1vh", backgroundColor: "rgba(41,41,41,0.1)", fontWeight:"bolder"}}>SpaceX Launch Programs</h1>)
+}
