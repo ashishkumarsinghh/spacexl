6732ba34ef200db5fb160fdcb21757ab2120bf4b
ashish kumar singh <ashishkumarsinghh@outlook.com>
2020-10-21 21:39:59 +0530

    add filters ui

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,35 @@
 import axios from "axios"
-import {Row, Col, Container, Card, Image } from "react-bootstrap"
+import {Row, Col, Container, Card, Button } from "react-bootstrap"
 import Header from "./header"
 export default function Home({launches}) {
+  const baseUrl = "https://api.spaceXdata.com/v3/launches?limit=100"
   return (
     <Container>
-      <Row><Col><Header/></Col></Row>
-      <Row>
-        <Col sm={12} md={2}>
-          Filters
+      <Row key="header"><Col><Header/></Col></Row>
+      <Row key="content">
+        <Col sm={12} md={2} key="filters">
+          <h4>Filters</h4>
+          <h6>Launch year</h6>
+          <Row key="yearfilters">
+            {[2006,2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020].map((year) => <Col style={{paddingBottom:"1vh"}}><Button variant="primary" size="sm">{year}</Button></Col>)}
+          </Row>
+          <hr/>
+          <h6>Succesful Launch</h6>
+          <Row key="ls">
+
+            <Col key="lst"><Button variant="success" size="sm">True</Button></Col>
+            <Col key="lsf"><Button variant="danger"  size="sm">False</Button></Col>
+          </Row>
+          <hr/>
+          <h6>Succesful Landing</h6>
+
+          <Row key="ll">
+            <Col key="llst"><Button variant="success" size="sm">True</Button></Col>
+            <Col key="llsf"><Button variant="danger"  size="sm">False</Button></Col>
+          </Row>
+
         </Col>
-        <Col sm={12} md={10}>
+        <Col sm={12} md={10} key="results">
         <Row>
         {launches.map(item => {
         return <Col sm={12} md={6} lg={3} style={{paddingBottom: "1vh"}}>
@@ -22,6 +42,9 @@ export default function Home({launches}) {
           <Card.Body>
         <p style={{fontWeight:"bold", fontSize:"0.8rem"}}>Launch Year: {item.launch_year}</p>
         <p style={{fontWeight:"bold", fontSize:"0.8rem"}}>Launching: {item.launch_success? "Successful": "Unsuccessful"}</p>
+        <p style={{fontWeight:"bold", fontSize:"0.8rem"}}>Landing: {item.rocket.first_stage.cores[0].land_success? "Successful": "Unsuccessful"}</p>
+
+
           </Card.Body>
           </Card>
           </Col>
