75a561eef91f32e1b2d7f92c5af09ae7d75e40f8
ashish kumar singh <ashishkumarsinghh@outlook.com>
2020-10-22 16:41:31 +0530

    apply filters for year and launching/landing

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,30 @@
 import axios from "axios"
+import { useState } from 'react';
 import {Row, Col, Container, Card, Button } from "react-bootstrap"
 import Header from "./header"
 export default function Home({launches}) {
   const baseUrl = "https://api.spaceXdata.com/v3/launches?limit=100"
+  const [items, setitems] = useState(launches)
+
+  const fetchByYear = async (e) => {
+    e.preventDefault()
+    const baseUrl = "https://api.spaceXdata.com/v3/launches?limit=100&launch_year="+e.target.id
+    const data = await axios.get(baseUrl)
+    setitems(data.data)
+  }
+
+  const fetchByLaunch= async (status) => {
+    console.log(status)
+    const baseUrl = "https://api.spaceXdata.com/v3/launches?limit=100&launch_success="+status
+    const data = await axios.get(baseUrl)
+    setitems(data.data)
+  }
+
+  const fetchByLand = async (status) => {
+    const baseUrl = "https://api.spaceXdata.com/v3/launches?limit=100&land_success="+status
+    const data = await axios.get(baseUrl)
+    setitems(data.data)
+  }
   return (
     <Container>
       <Row key="header"><Col><Header/></Col></Row>
@@ -11,28 +33,28 @@ export default function Home({launches}) {
           <h4>Filters</h4>
           <h6>Launch year</h6>
           <Row key="yearfilters">
-            {[2006,2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020].map((year) => <Col style={{paddingBottom:"1vh"}}><Button variant="primary" size="sm">{year}</Button></Col>)}
+            {[2006,2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020].map((year) => <Col key={year} style={{paddingBottom:"1vh"}}><Button variant="primary" size="sm" onClick={fetchByYear} id={year}>{year}</Button></Col>)}
           </Row>
           <hr/>
           <h6>Succesful Launch</h6>
           <Row key="ls">
 
-            <Col key="lst"><Button variant="success" size="sm">True</Button></Col>
-            <Col key="lsf"><Button variant="danger"  size="sm">False</Button></Col>
+            <Col key="lst"><Button variant="success" size="sm"  onClick={()=>fetchByLaunch(true)}>True</Button></Col>
+            <Col key="lsf"><Button variant="danger"  size="sm"  onClick={()=>fetchByLaunch(false)}>False</Button></Col>
           </Row>
           <hr/>
           <h6>Succesful Landing</h6>
 
           <Row key="ll">
-            <Col key="llst"><Button variant="success" size="sm">True</Button></Col>
-            <Col key="llsf"><Button variant="danger"  size="sm">False</Button></Col>
+            <Col key="llst"><Button variant="success" size="sm" onClick={()=>fetchByLand(true)}>True</Button></Col>
+            <Col key="llsf"><Button variant="danger"  size="sm"  onClick={()=>fetchByLaunch(false)}>False</Button></Col>
           </Row>
 
         </Col>
         <Col sm={12} md={10} key="results">
         <Row>
-        {launches.map(item => {
-        return <Col sm={12} md={6} lg={3} style={{paddingBottom: "1vh"}}>
+        {items.map(item => {
+        return <Col sm={12} md={6} lg={3} style={{paddingBottom: "1vh"}} key = {"col"+item.mission_id+item.mission_name}>
         <Card key = {item.mission_id+item.mission_name} bg="light">
 
 
